32cf53701bfd72359f99de9af4f400e1f7790d6a
faris <faisabuarram@outlook.sa>
2019-07-17 18:39:40 +0300

    FIrst Main Menu

---
+++ stickyNote/stickyNote/Source.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	int choice;// to control what is  the option to user need to move it 
+	do
+	{
+		cout << "Welcome to the brand new “Sticky Notes”!\nHere is the list of operation this program offers : \n 1 - Add new user \n 2 - Add new note \n 3 - View notes for a specific user \n 4 - Exit \n ";
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+		{
+
+		}
+		break;
+		case 2:
+		{
+
+		}
+		break;
+		case 3:
+		{
+
+		}
+		break;
+		case 4:
+			break;
+		}
+
+
+
+	} while (choice != 4);
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
