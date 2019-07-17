23b28dfcb018a42fc886590141596dd845cae601
faris <faisabuarram@outlook.sa>
2019-07-17 18:47:51 +0300

    Add Note Class

--- stickyNote/stickyNote/Source.cpp
+++ stickyNote/stickyNote/Source.cpp
@@ -1,10 +1,54 @@
+#pragma warning(disable : 4996)
 #include<iostream>
 #include<fstream>
+#include<string>
+#include <ctime>
+
 
 using namespace std;
+string monthString[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
 
+void getTm(int& year, int& month, int& day, int& hour, int& mins)
+{
+	time_t tt;
+	time(&tt);
+	tm TM = *localtime(&tt);
+
+	year = TM.tm_year + 1900;
+	month = TM.tm_mon;
+	day = TM.tm_mday;
+	hour = TM.tm_hour;
+	mins = TM.tm_min;
+
+}
+
+
+class  Note
+{
+private:
+	string note;//a note that user add it
+
+public:
+	Note() {}
+	Note(string note) {
+		this->note = note;
+	}
+
+	string toStringNote() {
+		int year, month, day, hour, mins;
+		getTm(year, month, day, hour, mins);
+		return (note + "\n" + to_string(day) + '/' + to_string(month) + '/' + to_string(year) + " " + to_string(hour) + ':' + to_string(mins) + ':');
+	}
+
+
+
+
+
+};
 int main()
 {
+
+
 	int choice;// to control what is  the option to user need to move it 
 	do
 	{
@@ -20,6 +64,7 @@ int main()
 		break;
 		case 2:
 		{
+
 
 		}
 		break;
