a37855ab820df2748893c469a5f7133d77c4eb87
faris <faisabuarram@outlook.sa>
2019-07-20 22:24:48 +0300

    update

--- stickyNote/stickyNote/Source.cpp
+++ stickyNote/stickyNote/Source.cpp
@@ -1,4 +1,3 @@
-
 /*This project was created to creat file carries user's name
 so, The user can write notes inside it with the time it was written
 
@@ -18,10 +17,10 @@ using namespace std;
 void printUserMenu()
 {
 	cout << "Welcome to the brand new Sticky Notes!\n\n"
-		<< "Here is the list of operation this program offers : \n"
-		<< " 1 - Add new user \n 2 - Add new note \n "
-		<< "3 - View notes for a specific user \n 4 - Exit \n\n"
-		<< "Please, enter your choice: ";
+			 << "Here is the list of operation this program offers : \n"
+			 << " 1 - Add new user \n 2 - Add new note \n "
+			 << "3 - View notes for a specific user \n 4 - Exit \n\n"
+			 << "Please, enter your choice: ";
 }
 void pressEnter()
 {
@@ -38,80 +37,34 @@ void pressEnter()
     }while(1);
 }
 // This function will implement the first option in main menu
-<<<<<<< HEAD
 void addNewUser()
-=======
-void addingNewUser() 
->>>>>>> 1d995f829404c5f3baa77bdf94211ea2443a14db
 {
 	/*
-	This fuction let the user to add new user
-	by entering first name and last name.
-	Adding new user means open a new file in specific directory.
-	Project guarantee that there is a unique user.
-	*/
+				This fuction let the user to add new user
+				by entering first name and last name.
+				Adding new user means open a new file in specific directory.
+				Project guarantee that there is a unique user.
+				*/
 
-	cout << "Welcome aboard new user!\nPlease let me know your first name: ";
-	string firstName;				cin >> firstName;
+				cout << "Welcome aboard new user!\nPlease let me know your first name: ";
+				string firstName;				cin >> firstName;
 
-	cout << "Great " << firstName << ", now please enter your last name: ";
-	string lastName;				cin >> lastName;
+				cout << "Great " << firstName << ", now please enter your last name: ";
+				string lastName;				cin >> lastName;
 
-	string fullName = firstName + "" + lastName;
-	ifstream inUserFile (fullName);
+				string fullName = firstName + "" + lastName;
+				ifstream inUserFile (fullName);
 
-	if (!inUserFile) // there is no file have the same user name
-		ofstream outUserFile (fullName); // just to open new file have the same name of user
-	else // user name was used
-		cout << "\n\n" << fullName << " was used!\n please try another name or write your notes directly!\n";
-
-}
-
-// This function will implement the third option in main menu
-void printAllNotes()
-{
-	cout << "Retrieve your notes? Absolutely! Please let know your full name first: ";
-
-	string firstName , lastName;
-	cin >> firstName >> lastName;
-
-	string fullName = firstName + "" + lastName;
-	ifstream inUserFile (fullName);
-
-	if (!inUserFile) // there is no file have the same user name
-		cout << "\n\n" << "User name: " << fullName << "was not found, please try diffrent user name!\n";
-	else // user name was found
-	{
-		cout << "Found it!\n"
-			 << "Here are your stored notes:\n"
-			 << "-------------\n";
-
-		string fileContents;
-		while (inUserFile >> fileContents )
-			cout << fileContents << " ";
-		
-		cout << "-------------\n"
-			 << "Happy to serve you :)\n";
-
-
-	}
-
-
-
+				if (!inUserFile) // there is no file have the same user name
+					ofstream outUserFile (fullName); // just to open new file have the same name of user
+				else // user name was used
+					cout << "\n\n" << fullName << " was used!\n please try another name or write your notes directly!\n";
 
 }
 
-<<<<<<< HEAD
 
 
 class  Note
-=======
-
-
-string monthString[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };// array of string contains the  months
-
-void getTime(int& year, int& month, int& day, int& hour, int& mins)
->>>>>>> 1d995f829404c5f3baa77bdf94211ea2443a14db
 {
         // class to add new note
     private:
@@ -130,16 +83,11 @@ void getTime(int& year, int& month, int& day, int& hour, int& mins)
                 hour = TM.tm_hour;
                 mins = TM.tm_min;
 
-<<<<<<< HEAD
         }
-=======
-}
->>>>>>> 1d995f829404c5f3baa77bdf94211ea2443a14db
 
     public:
         Note() {}// default constructure
 
-<<<<<<< HEAD
 
         Note(string note)
         {
@@ -185,16 +133,6 @@ void addNewNote()
 
 
                 }
-=======
-public:
-	Note() {}// default constructure
-
-	Note(string note)
-	{
-		//constructure was take note as  a parameters
-		this->note = note;
-	}
->>>>>>> 1d995f829404c5f3baa77bdf94211ea2443a14db
 
 
 }
@@ -219,61 +157,22 @@ int main()
 				Adding new user means open a new file in specific directory.
 				Project guarantee that there is a unique user.
 				*/
-				addingNewUser();
+				addNewUser();
 				system("pause");
 
 			}
 			break;
 		case 2:
 			{
-				/*
+			    /*
 				This option let the user to add new note
 				by entering first name and last name.
 				Adding new note means open a specific file depend on user name if exist or not .
 
 				*/
-<<<<<<< HEAD
 				addNewNote();
 				pressEnter();
 
-=======
-				string firstName, lastName;//user's full name
-				cout << "Let�s add a new note ... \n";
-				cout << "Please enter your full name first:  "; cin >> firstName >> lastName;
-				string fullName = firstName + lastName + ".txt";
-				ifstream file(fullName);
-				if (!file)
-				{
-					cout << "Oh! Sorry the user name was not found, please check the name again and if this is your first time here, please go ahead and create a new user from the main menu ..." << endl;
-
-				}
-				else
-				{
-
-					cout << "Your record is found, I�m now opening your file �.\nReady!\nPlease enter your note :";
-					cin.ignore();
-					string newNote;
-					cin.ignore();
-					getline(cin, newNote);
-					Note note(newNote);
-					ofstream File;
-					File.open(fullName, ios_base::app);
-					File << note.toStringNote();
-					File.close();
-
-
-				}
-				cout << "Press Enter to return to main menu : ";
-				cin.ignore();
-				char ch;
-				cin.get(ch);
-				while(ch!='\n')
-				{
-					cin.ignore();
-					cout << "Press Enter to return to main menu : ";
-					cin.get(ch);
-				}
->>>>>>> 1d995f829404c5f3baa77bdf94211ea2443a14db
 
 
 			}
@@ -287,7 +186,7 @@ int main()
 			break;
 		}
 
-		system("cls");
+            system("cls");
 
 	} while (userChoice != 4);
 
